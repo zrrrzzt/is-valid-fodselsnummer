d7ac95a67f696550b96bb61b4995e825fbb8fe2f
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-07-17 23:17:15 +0200

    Fixes to please jscs

--- test/isValidFodselsnummer.js
+++ test/isValidFodselsnummer.js
@@ -3,16 +3,25 @@
 var tap = require('tap');
 var isValidFodselsnummer = require('../index.js');
 
-tap.equal(true, isValidFodselsnummer('01010750160'), '01010750160 is a valid ID-number');
+tap.equal(
+  true, isValidFodselsnummer('01010750160'),
+  '01010750160 is a valid ID-number'
+);
 
-tap.equal(false, isValidFodselsnummer('12341234567'), '12341234567 is not a valid ID-number');
+tap.equal(
+  false, isValidFodselsnummer('12341234567'),
+  '12341234567 is not a valid ID-number'
+);
 
 tap.throws(
   function() {
-    isValidFodselsnummer('1234123456')
+    isValidFodselsnummer('1234123456');
   },
   {message: 'Too short. Expected length of 11.'},
   'Throws if number too short'
 );
 
-tap.equal(true, isValidFodselsnummer('41085801188'), '41085801188 is a valid ID-number');
+tap.equal(
+  true, isValidFodselsnummer('41085801188'),
+  '41085801188 is a valid ID-number'
+);
